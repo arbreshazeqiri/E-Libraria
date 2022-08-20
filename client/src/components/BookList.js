5d18ac915ba564dbeb1ca11d5ac58ead18c73d80
Nafie Sallahu <nafiesallahu1@gmail.com>
2022-08-20 15:57:35 +0200

    Sort by rating & alphabet added

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
 import Categories from "./Categories";
+import Dropdown from "react-bootstrap/Dropdown";
+import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 import "./BookList.css";
 
@@ -11,22 +13,45 @@ const BookList = () => {
   const [genres] = useState([]);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/api/books")
-      .then((res) => {
-        setBooks(res.data);
-        Object.keys(res.data).forEach(function (key, index) {
-          if (!authors.includes(res.data[key].author)) {
-            authors.push(res.data[key].author);
-          }
-          if (!genres.includes(res.data[key].genre)) {
-            genres.push(res.data[key].genre);
-          }
-        });
-      })
-      .catch((err) => console.log(err));
-  }, [authors, genres]);
+  useEffect(
+    (sort) => {
+      axios
+        .get("http://localhost:8000/api/books")
+        .then((res) => {
+          setBooks(res.data);
+          Object.keys(res.data).forEach(function (key, index) {
+            if (!authors.includes(res.data[key].author)) {
+              authors.push(res.data[key].author);
+            }
+            if (!genres.includes(res.data[key].genre)) {
+              genres.push(res.data[key].genre);
+            }
+          });
+        })
+        .catch((err) => console.log(err));
+    },
+    [authors, genres, setBooks]
+  );
+
+  const sortAlphabeticallyDown = () => {
+    setBooks(
+      [...books].sort((a, b) =>
+        a.title.toUpperCase().localeCompare(b.title.toUpperCase())
+      )
+    );
+  };
+
+  const sortAlphabeticallyUp = () => {
+    setBooks(
+      [...books].sort((a, b) =>
+        b.title.toUpperCase().localeCompare(a.title.toUpperCase())
+      )
+    );
+  };
+
+  const sortByRating = () => {
+    setBooks([...books].sort((a, b) => b.rating - a.rating));
+  };
 
   return (
     <div className="even-more-container">
@@ -58,6 +83,25 @@ const BookList = () => {
       >
         EXPLORE OUR DIGITAL LIBRARY
       </h2>
+      <Dropdown
+        className="sort-icon"
+        style={{ alignSelf: "flex-end", marginRight: "4em" }}
+      >
+        <Dropdown.Toggle className="inner-sort-icon" style={{ height: "20px" }}>
+          Sort
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#" onClick={sortAlphabeticallyDown}>
+            Sort alphabetically A-Z
+          </Dropdown.Item>
+          <Dropdown.Item href="#" onClick={sortAlphabeticallyUp}>
+            Sort alphabetically Z-A
+          </Dropdown.Item>
+          <Dropdown.Item href="#" onClick={sortByRating}>
+            Sort by rating (highest to lowest)
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
       <div className="container">
         {books.map((book) => (
           <div key={book._id} className="book">
@@ -76,13 +120,15 @@ const BookList = () => {
       </div>
       <Categories genres={genres} />
 
-      <h2 style={{ marginBottom: "1em" }}>
-        FILTER BOOKS BY AUTHOR
-      </h2>
+      <h2 style={{ marginBottom: "1em" }}>FILTER BOOKS BY AUTHOR</h2>
       <div className="users-container">
         {authors.map((author, i) => (
           <div className="user-card" key={i}>
-            <button type="button" className="btn btn-light" onClick={(e) => navigate('/filter-authors/' + author)}>
+            <button
+              type="button"
+              className="btn btn-light"
+              onClick={(e) => navigate("/filter-authors/" + author)}
+            >
               {author}
             </button>
           </div>
@@ -90,8 +136,8 @@ const BookList = () => {
       </div>
 
       <h3 className="pre-footer">
-        Be part of the community that is transforming reading one
-        book at a time.
+        Be part of the community that is transforming reading one book at a
+        time.
       </h3>
     </div>
   );
